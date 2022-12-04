e97af038993fab2dbe786ebae1c5c08f684f47ef
Lanist99 <danilevic99@gmail.com>
2022-12-04 22:40:15 +0100

    add search clothes
    added search , filter, dynamic append good

--- Wildberris/js/getGoods.js
+++ Wildberris/js/getGoods.js
@@ -1,20 +1,68 @@
 const getGoods = () => {
-    const links = document.querySelectorAll('.navigation-link')
+    const links = document.querySelectorAll('.navigation-link')    
+// draw the received data
+    const renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list')
+       
+        goodsContainer.innerHTML = ''
 
-    const getData = () => {
+        goods.forEach(good => {
+           const goodBlock =document.createElement('div')
+
+           
+
+           goodBlock.classList.add('col-lg-3')
+           goodBlock.classList.add('col-sm-6')
+
+           goodBlock.innerHTML = ` 
+					<div class="goods-card">
+						<span class="label ${good.label ? null :  'd-none'}">${good.label}</span>
+		                <img src="db/${good.img}" alt="${good.name}" class="goods-image">				
+						<h3 class="goods-title">${good.name}</h3>
+						<p class="goods-description">${good.description}</p>
+						<button class="button goods-card-btn add-to-cart" data-id="007">
+							<span class="button-price">$${good.price}</span>
+						</button>
+					</div>
+           `
+          goodsContainer.append(goodBlock)
+
+        //   console.log(good)
+        })
+    }
+
+    const getData = (value, category) => {
         fetch('db/db.json')
             .then((res) => res.json()) 
             .then((data) => {
-                console.log(data);
+                const array = category ? data.filter((item) =>  item[category] ===value) : data
+
+                localStorage.setItem('goods',JSON.stringify(array))
+                
+                if (window.location.pathname !== 'goods.html') {
+                    window.location.href = 'goods.html'
+                } else {
+                    renderGoods(array)
+                }
+                // window.location.href = 'goods.html'
+                // console.log(window.location)
             })
     }
     
     links.forEach((link) => {
        link.addEventListener('click', (event) => {
             event.preventDefault()
-            getData()
+            const linkValue = link.textContent
+            // console.log(linkValue)
+            const category = link.dataset.field
+            getData(linkValue, category)
+
        })
     })
+
+    if (localStorage.getItem('goods')) {
+        renderGoods(JSON.parse(localStorage.getItem('goods')))
+    }
 }
 
 getGoods()
--- Wildberris/js/search.js
+++ Wildberris/js/search.js
@@ -1,10 +1,59 @@
 const search =  function () {
-    const input = document.querySelector('.search-block > input')
-    const searchBtn = document.querySelector('.search-block > button')
+  const input = document.querySelector('.search-block > input')
+  const searchBtn = document.querySelector('.search-block > button')
     
-   searchBtn.addEventListener('click',() =>{
-       console.log(input.value);
-     })
+  const renderGoods = (goods) => {
+    const goodsContainer = document.querySelector('.long-goods-list')
+   
+    goodsContainer.innerHTML = ''
+
+    goods.forEach(good => {
+       const goodBlock =document.createElement('div')
+
+       
+
+       goodBlock.classList.add('col-lg-3')
+       goodBlock.classList.add('col-sm-6')
+
+       goodBlock.innerHTML = ` 
+      <div class="goods-card">
+        <span class="label ${good.label ? null :  'd-none'}">${good.label}</span>
+                <img src="db/${good.img}" alt="${good.name}" class="goods-image">				
+        <h3 class="goods-title">${good.name}</h3>
+        <p class="goods-description">${good.description}</p>
+        <button class="button goods-card-btn add-to-cart" data-id="007">
+          <span class="button-price">$${good.price}</span>
+        </button>
+      </div>
+       `
+      goodsContainer.append(goodBlock)
+
+      // console.log(good)
+    })
+}
+
+const getData = (value, category) => {
+    fetch('db/db.json')
+        .then((res) => res.json()) 
+        .then((data) => {
+            const array = data.filter(good => {
+              // search inFullText lowerCase
+              return good.name.toLowerCase().includes(value.toLowerCase())
+            })
+            localStorage.setItem('goods',JSON.stringify(array))
+            
+            if (window.location.pathname !== 'goods.html') {
+                window.location.href = 'goods.html'
+            } else {
+                renderGoods(array)
+            }
+           
+        })
+} 
+
+  searchBtn.addEventListener('click',() =>{
+       getData(input.value)
+      })
 }
 
 search()
