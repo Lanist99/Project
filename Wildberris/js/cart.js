acd2a6c3ff159dac82cfefa59e130add2b5724ae
Lanist99 <danilevic99@gmail.com>
2022-12-06 15:15:21 +0100

    get a price

--- Wildberris/js/cart.js
+++ Wildberris/js/cart.js
@@ -2,6 +2,11 @@ const cart = function() {
     const cartBtn = document.querySelector(".button-cart")
     const cart = document.getElementById("modal-cart")
     const closeBtn = document.querySelector(".modal-close")
+    const goodsContainer = document.querySelector(".long-goods-list")
+
+    // const addToCart = () => {
+        
+    // }
 
     cartBtn.addEventListener("click", function() {
     cart.style.display = "flex"
@@ -11,5 +16,18 @@ const cart = function() {
         cart.style.display = ""
     })
 
+    if (goodsContainer) {
+        goodsContainer.addEventListener('click', (event) => {
+            if (event.target.closest('.add-to-cart')) {
+                const buttonToCart = event.target.closest('.add-to-cart')
+                const goodId = buttonToCart.dataset.id
+                const goods = JSON.parse(localStorage.getItem('goods'))
+                // const clickedGood = goods.find(good => good.id === goodId)
+                // const card = localStorage.getItem('cart') ?
+                //     JSON.parse(localStorage.getItem('cart')): []
+                console.log(goods);
+            }
+        })
+    }
 }
 cart()
